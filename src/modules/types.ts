189cf8452f1cacb4361f94b24f122852764d7ecf
Grzesiek <dadmor@gmail.com>
2025-03-16 01:42:05 +0100

    autocommit 0.25.11

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -111,13 +111,3 @@ export interface FlowData {
   edges: ReactFlowEdge[];
 }
 
-// Common type definitions for action creators
-
-import { Draft } from "immer";
-import { AppState } from "../types";
-
-// The set function from immer middleware takes a function that modifies the draft state
-export type SetFn = (fn: (state: Draft<AppState>) => void) => void;
-
-// The get function returns the current state
-export type GetFn = () => AppState;
