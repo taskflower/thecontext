7359e6790b1aad504c870c2fb02643893ac7148e
Grzesiek <dadmor@gmail.com>
2025-05-07 21:57:28 +0200

    autocommit 0.50.54

--- src/themes/clean/components/WidgetsStep.tsx
+++ src/themes/clean/components/WidgetsStep.tsx
@@ -75,7 +75,7 @@ export default function WidgetsStep({
         </div>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
         {widgets.map(loadWidget)}
       </div>
 
