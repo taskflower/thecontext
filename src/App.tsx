d6d82a6460d351234806b4b041eb52ab39877764
Grzesiek <dadmor@gmail.com>
2025-05-25 13:57:52 +0200

    autocommit 0.59.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 /// src/App.tsx
-import { BrowserRouter, useRoutes } from "react-router-dom";
+import { BrowserRouter, useParams, useRoutes } from "react-router-dom";
+import { useConfig } from "./hooks";
+import React from "react";
 import ConfigPage from "./pages/ConfigPage";
-import Layout from "./layout/Layout";
 
 function AppRoutes() {
   const routes = [
@@ -13,11 +14,21 @@ function AppRoutes() {
 }
 
 export function App() {
+  const { config } = useParams<{ config: string }>();
+  const cfgName = config || "testApp";  // Domyślnie 'testApp' jeśli brak 'config'
+  const app = useConfig<any>(cfgName, `/src/!CONFIGS/${cfgName}/app.json`);
+  const theme = app?.tplDir || 'test'; // Użyj domyślnego motywu jeśli nie ma
+
+  // Ładowanie layoutu z motywu
+  const Layout = React.lazy(() => import(`./themes/${theme}/layouts/Simple`));
+
   return (
     <BrowserRouter>
-      <Layout>
-        <AppRoutes />
-      </Layout>
+      <React.Suspense fallback={<div>Loading layout...</div>}>
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </React.Suspense>
     </BrowserRouter>
   );
 }
