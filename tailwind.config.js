c620426ce74a69cddc1890581dcdcd8107d77dcb
Grzesiek <dadmor@gmail.com>
2025-06-02 17:01:07 +0200

    0.66.8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-
   content: [
     "./index.html",
     './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@stoplight/json-schema-viewer/**/*.{js,css}',
-    './node_modules/@stoplight/mosaic/**/*.{js,css}',
   ],
   theme: {
     extend: {
